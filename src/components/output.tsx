1fb09a70e33b83db207d55e52f400daf3b7f5ed7
sarassaura <sarahyukinonakada@gmail.com>
2023-12-13 18:09:25 -0300

    first effect

--- src/components/output.tsx
+++ src/components/output.tsx
@@ -1,7 +1,12 @@
+import { createEffect } from 'solid-js';
 import { useCode } from '../context/code';
 
 export default function Output() {
 	const [code] = useCode();
+
+	createEffect(() => {
+		console.log('The code is: ', code());
+	});
 
 	return <div class='output'>{code()}</div>;
 }
