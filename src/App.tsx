6af6ffe5baff864a13f7be7b623ad43fd72a76ad
sarassaura <sarahyukinonakada@gmail.com>
2023-12-12 22:10:35 -0300

    improvement

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,10 @@ import Output from './components/output';
 const App: Component = () => {
   return (
     <>
-      <header class='bg-slate-700'>
-        <nav class='flex justify-between'>
-          <p class='p-4'>Code Editor</p>
-        </nav>
-      </header>
       <main>
         <Editor />
         <Output />
       </main>
-      <footer class='bg-slate-700 flex justify-center'>
-        <p class='p-4'>©sarassaura, 2023. All rights reserved.</p>
-      </footer>
     </>
   );
 };
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,7 +1,22 @@
 export default function Editor() {
-    return (
-        <pre class="editor" contentEditable spellcheck={false}>
-            Hello
-        </pre>
-    )
+	return (
+		<div class='wrapper'>
+			<div class='m-3 flex gap-2'>
+				<div class='tabs border'>javascript</div>
+				<div class='tabs'>markdown</div>
+				<div class='tabs'>html</div>
+				<div class='tabs'>css</div>
+			</div>
+			<textarea
+				class='editor'
+				contentEditable
+				spellcheck={false}
+				onChange={(e) => {
+					console.log(e);
+				}}
+			>
+				Hello
+			</textarea>
+		</div>
+	);
 }
